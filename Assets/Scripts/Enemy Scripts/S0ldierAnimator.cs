f8ad7ca40c6664f57012e4348ca7dd727ae51b55
malicious233 <alfons.hedstrom@gmail.com>
2021-11-02 12:56:28 +0100

    - Implemented aim constraint to the S0ldier - Sets and unsets rigweights wether or not it is agitated

--- Assets/Scripts/Enemy Scripts/S0ldierAnimator.cs
+++ Assets/Scripts/Enemy Scripts/S0ldierAnimator.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations.Rigging;
 
 public class S0ldierAnimator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Transform aimTarget;
+    [SerializeField] Animator anim;
+    [SerializeField] Rig rig;
+    EnemyAI AI;
+    
+    
+
+    private void Awake()
     {
+        AI = GetComponent<EnemyAI>();
+        aimTarget.parent = null;
+    }
+
+    private void FixedUpdate()
+    {
+        UpdateAimTargetPosition();
+    }
+
+    private void UpdateAimTargetPosition()
+        //Updates the aim target transform position which the rig constraint uses
+    {
+        if (AI.target == null)
+        {
+            SetRigWeights(false);
+        }
+        else
+        {
+            SetRigWeights(true);
+            aimTarget.position = AI.target.position;
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SetRigWeights(bool _trueOrFalse)
     {
-        
+        if (_trueOrFalse)
+        {
+            rig.weight = 1;
+        }
+        else
+        {
+            rig.weight = 0;
+        }
     }
 }
