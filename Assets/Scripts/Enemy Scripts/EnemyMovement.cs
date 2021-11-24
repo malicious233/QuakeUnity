aa0d059f49c0ea974c0d6798cf0f0c5154bc7b71
malicious233 <alfons.hedstrom@gmail.com>
2021-11-24 16:49:24 +0100

    - Enemy Movement overhauling. This is only the start of it and it currently doesn't work. I shall add another component which handles the pathfinding input

--- Assets/Scripts/Enemy Scripts/EnemyMovement.cs
+++ Assets/Scripts/Enemy Scripts/EnemyMovement.cs
@@ -3,7 +3,78 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class EnemyMovement : MonoBehaviour
+public class EnemyMovement : MonoBehaviour, IMoveable
 {
+    CharacterController controller;
+    public Vector3 velocity;
+    public Vector3 inputVector;
+
+    [Header("STATS:")]
+    [SerializeField] float gravity;
+    [SerializeField] float movementSpeed;
+    [SerializeField] float deaccelSpeed;
+
+    public Vector3 Velocity
+    {
+        get
+        {
+            return velocity;
+        }
+        set
+        {
+            velocity = value;
+        }
+    }
+
+    #region Custom Methods
+    public Vector3 UpdateVelocityGround(Vector3 _currentVelocity)
+    {
+        _currentVelocity = Friction(_currentVelocity);
+        //_currentVelocity.y = 0;
+        return _currentVelocity;
+    }
+    
+    public Vector3 UpdateVelocityAir(Vector3 _currentVelocity)
+    {
+        
+        return _currentVelocity;
+    }
+
+    Vector3 Friction(Vector3 _currentVelocity)
+    {
+        _currentVelocity -= _currentVelocity * deaccelSpeed * Time.deltaTime;
+        return _currentVelocity;
+    }
+
+    Vector3 ApplyGravity(Vector3 _currentVelocity)
+    {
+        _currentVelocity.y -= gravity;
+        return _currentVelocity;
+    }
+    #endregion
+
+
+    #region Unity Methods
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (controller.isGrounded)
+        {
+            
+            velocity = UpdateVelocityGround(velocity);
+        }
+        else
+        {
+            velocity = ApplyGravity(velocity);
+            velocity = UpdateVelocityAir(velocity);
+        }
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+    #endregion
 
 }
