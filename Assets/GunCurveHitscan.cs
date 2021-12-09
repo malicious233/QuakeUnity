63952959f79b727df27ede4fa9b21ff915241ee8
malicious233 <alfons.hedstrom@gmail.com>
2021-12-09 16:46:46 +0100

    - Added laserpointer for CRV

--- Assets/GunCurveHitscan.cs
+++ Assets/GunCurveHitscan.cs
@@ -5,15 +5,60 @@ using MyBezier;
 
 public class GunCurveHitscan : GunFire
 {
+    LineRenderer line;
+    Material mat;
+
+    [SerializeField] GunDamage shotDamage;
+
     [Header("CURVE PROPERTIES:")]
     [SerializeField] Transform gunCurveAnchor;
     [SerializeField] Transform forwardCurveAnchor;
     [SerializeField] Transform magnetCurveAnchor;
     [SerializeField] int curveGranularity = 30;
 
-    [SerializeField] GameObject tst;
+    [Header("MATERIAL PROPERTIES:")]
+    [SerializeField] Material curveHitMat;
+    [SerializeField] Material curveNoHitMat;
 
-    [SerializeField] GunDamage shotDamage;
+    Vector3[] pointerLinePositions;
+
+    public override void Awake()
+    {
+        base.Awake();
+        line = GetComponent<LineRenderer>();
+
+    }
+
+    private void Update()
+    {
+        pointerLinePositions = new Vector3[curveGranularity];
+
+        Vector3 prev = gunCurveAnchor.position;
+        for (int i = 0; i < curveGranularity; i++)
+        {
+            float v = i / (curveGranularity - 1f);
+            Vector3 p = Bezier.CalculateQuadraticBezierPoint(v, gunCurveAnchor.position, forwardCurveAnchor.position, magnetCurveAnchor.position);
+
+            //Gizmos.DrawLine(prev, p);
+            pointerLinePositions[i] = p;
+            prev = p;
+
+        }
+        line.positionCount = curveGranularity;
+        line.SetPositions(pointerLinePositions);
+
+        RaycastHit hit;
+        if (Bezier.CurveRaycast(gunCurveAnchor.position, forwardCurveAnchor.position, magnetCurveAnchor.position, curveGranularity, StaticVariables.hurtboxMask, out hit))
+        {
+            line.material = curveHitMat;
+        }
+        else
+        {
+            line.material = curveNoHitMat;
+        }
+
+
+    }
 
     public void OnDrawGizmos()
     {
@@ -35,7 +80,7 @@ public class GunCurveHitscan : GunFire
     {
         base.Shoot();
         RaycastHit hit;
-        if (Bezier.CurveRaycast(gunCurveAnchor.position, forwardCurveAnchor.position, magnetCurveAnchor.position, 30, hittableMask, out hit))
+        if (Bezier.CurveRaycast(gunCurveAnchor.position, forwardCurveAnchor.position, magnetCurveAnchor.position, curveGranularity, hittableMask, out hit))
         {
             Debug.Log("SUCCESS");
             //Instantiate(tst, hit.point, Quaternion.identity);
